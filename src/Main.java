16179b6fa21134e66d5f82508e9f30f2efbae421
Yura <yura.mkrtchyan01@gmail.com>
2022-02-04 12:57:53 +0400

    day 2 homework

---
+++ src/Main.java
@@ -0,0 +1,120 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//  task 2. Write a Java program to convert minutes into a number of hours,
+//  print count of hours and minutes. Input minutes from console
+//        System.out.println("Please enter minutes: ");
+//        int minutes = scanner.nextInt();
+//        int hours = minutes / 60;
+//        int remainingMinutes = minutes % 60;
+//        System.out.println(hours + ":" + remainingMinutes);
+
+//  task 3.	Find the sum of
+//  -the first 4 consecutive odd numbers
+//  -the first 4 consecutive even numbers
+//        while (true) {
+//            int firstNumber = scanner.nextInt();
+//            System.out.println(4 * firstNumber + 2 + 4 + 6);
+//        }
+
+//        int firstNum = scanner.nextInt();
+//        int secondNum = scanner.nextInt();
+//        int thirdNum = scanner.nextInt();
+//  task 4. Write a Java program  that takes two numbers as input from the console
+//  and display the product of two numbers
+//        System.out.println(firstNum * secondNum);
+//
+//  Task 5. Input from console 3 arguments and then prints the sum,the average
+//        float sum = firstNum + secondNum + thirdNum;
+//        float avg = sum/3;
+//        System.out.println("sum = " + sum + ", average = " + avg);
+
+//  Task 6.1. right-angled-triangle area
+//        float a = scanner.nextInt();
+//        float b = scanner.nextInt();
+//        float triangleArea = a * b / 2;
+//        System.out.println(triangleArea);
+
+//  Task 6.2. square area
+//        int c = scanner.nextInt();
+//        int squareArea = c * c;
+//        System.out.println(squareArea);
+
+//  Task 6.3. rectangle area
+//        int d = scanner.nextInt();
+//        int e = scanner.nextInt();
+//        int rectangleArea = d * e;
+//        System.out.println(rectangleArea);
+
+//  Task 7. In program we have byte b, short c , int a
+//  variables, print result of these actions
+//        byte b = scanner.nextByte();
+//        short c = scanner.nextShort();
+//        int a = scanner.nextInt();
+//        System.out.println("average = " + ((float) (a + b + c) / 3) + ", product = " + a * b * c);
+
+//  Task 8. Given arithmetic sequence Sn  = 3n*n - 12n
+//-	print the sum S10
+//-	print the sum S15
+//        int n = 10;
+//        System.out.println(3 * n * n - 12 * n);
+
+//  Task 9. Given int number,input from console, find last digit and print it
+//        int n = scanner.nextInt();
+//        System.out.println(n % 10);
+
+//  Task 11.Given int numbers ,input a and b from console,
+//  determine and print the remainder from  a to b
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int remainder = a % b;
+//        System.out.println(remainder);
+
+//  Task 12. Generate and print random integer number
+        Random random = new Random();
+//
+//        int number = random.nextInt(100);
+//        System.out.println(number);
+
+
+//  Task 13. Generate and print random integer number between 0 to 15(bound)
+//        System.out.println(random.nextInt(16));
+
+//  Task 14. Generate and print random integer number between 15 to 30
+//        System.out.println(random.nextInt(16) + 15);
+
+//  Task 15. Write java program which determines if the given int number is odd,
+//  print result(use == operator, use boolean primitive type).
+//        int num = scanner.nextInt();
+//        boolean isOdd = num % 2 == 1;
+//        System.out.println(isOdd ? "bool" : "not odd");
+
+//  16. Given an integer 3-digit number, determine and print the second digit. Input number from console.
+//        int number = scanner.nextInt();
+//        System.out.println(number / 10 % 10);
+
+//  Task 17. Given an integer  3-digit number, determine and print the first digit. Input number from console.
+//        int number = scanner.nextInt();
+//        System.out.println(number / 100);
+
+
+//  Task 18. Given an integer 3-digit number, determine and print the sum of all 3 digits. Input number from console.
+//        int number = scanner.nextInt();
+//        System.out.println(number % 10 + number / 10 % 10 + number / 100);
+
+
+//  19. Given 2 integer numbers, swap them,Input numbers from console
+//        int temp;
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        temp = a;
+//        a = b;
+//        b = temp;
+//        System.out.println("a=" + a + " b="+b);
+    }
+}
+
+
